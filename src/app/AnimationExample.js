45a3b1233e6eae9e60e4ff814b4e30d54e864c0f
niralipbytes <nirali.parikh@bytestechnolab.in>
2020-03-18 14:43:58 +0530

    Routing Basics

---
+++ src/app/AnimationExample.js
@@ -0,0 +1,12 @@
+import "./packages/react-router-dom/examples/Animation/styles.css";
+
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useLocation,
+    useParams
+} from "react-router-dom";
